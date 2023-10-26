970359a8a8890d21570e41e8ac5a6af885b7fa68
Arti2323 <123077179+Arti2323@users.noreply.github.com>
2023-10-26 16:40:23 +0300

    Update task.js

---
+++ dom/reveal/task.js
@@ -0,0 +1,34 @@
+const revealElements = document.querySelectorAll('.reveal');
+ 
+ 
+ 
+function isInViewport(element) {
+ 
+  const rect = element.getBoundingClientRect();
+ 
+  return (
+ 
+    rect.top >= 0 &&
+ 
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+ 
+  );
+ 
+}
+ 
+ 
+ 
+ 
+function handleScroll() {
+ 
+  revealElements.forEach(element => {
+ 
+    if (isInViewport(element)) {
+ 
+      element.classList.add('reveal_active');
+ 
+    }
+ 
+  });
+ 
+}
